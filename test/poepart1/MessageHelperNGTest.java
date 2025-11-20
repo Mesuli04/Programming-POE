edea8b640d243dcb915bedc9d7465b17ecf564e9
Mesuli04 <>
2025-11-20 02:19:21 +0200

    Part 3 commit 4

---
+++ test/poepart1/MessageHelperNGTest.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package poepart1;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author ICT 2022
+ */
+public class MessageHelperNGTest {
+    
+    public MessageHelperNGTest() {
+    }
+
+    /**
+     * Test of displaySentMessagesSendersRecipients method, of class MessageHelper.
+     */
+    @Test
+    public void testDisplaySentMessagesSendersRecipients() {
+    }
+
+    /**
+     * Test of displayLongestSentMessage method, of class MessageHelper.
+     */
+    @Test
+    public void testDisplayLongestSentMessage() {
+    }
+
+    /**
+     * Test of searchMessageByID method, of class MessageHelper.
+     */
+    @Test
+    public void testSearchMessageByID() {
+    }
+
+    /**
+     * Test of searchMessagesByRecipient method, of class MessageHelper.
+     */
+    @Test
+    public void testSearchMessagesByRecipient() {
+    }
+
+    /**
+     * Test of deleteMessageByHash method, of class MessageHelper.
+     */
+    @Test
+    public void testDeleteMessageByHash() {
+    }
+
+    /**
+     * Test of displaySentMessagesReport method, of class MessageHelper.
+     */
+    @Test
+    public void testDisplaySentMessagesReport() {
+    }
+
+    /**
+     * Test of showMessageHelperMenu method, of class MessageHelper.
+     */
+    @Test
+    public void testShowMessageHelperMenu() {
+    }
+
+    /**
+     * Test of getSentMessages method, of class MessageHelper.
+     */
+    @Test
+    public void testGetSentMessages() {
+    }
+
+    /**
+     * Test of getDisregardedMessages method, of class MessageHelper.
+     */
+    @Test
+    public void testGetDisregardedMessages() {
+    }
+
+    /**
+     * Test of getStoredMessages method, of class MessageHelper.
+     */
+    @Test
+    public void testGetStoredMessages() {
+    }
+
+    /**
+     * Test of getMessageHash method, of class MessageHelper.
+     */
+    @Test
+    public void testGetMessageHash() {
+    }
+
+    /**
+     * Test of getMessageID method, of class MessageHelper.
+     */
+    @Test
+    public void testGetMessageID() {
+    }
+    
+}
